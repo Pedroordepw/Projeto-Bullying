80d7f60ea790a8657ad5503971709e5875b109b2
Pedro <pedrohdelw@gmail.com>
2023-11-17 16:46:29 -0300

    Add Footer component and update Header component

--- aprendendo-sem-bullying/src/components/Footer.tsx
+++ aprendendo-sem-bullying/src/components/Footer.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from 'react';
+import logoImage from '../public/logo_project.png';
+import logoIfsul from '../public/ifrs.png';
 
 const Footer = () => {
   return (
-    <div>
-        <h1 className="text-">Footer</h1>
+    <div className='bg-header px-7 py-7'>
+      <div>
+        <ul className='flex flex-col md:flex-row justify-between items-center p-5'>
+          <li className='mb-4 md:mb-0'>
+            <img src={logoImage} alt='Project Logo image' className='h-14' />
+          </li>
+          <li className='mb-4 md:mb-0'>
+            <img src={logoIfsul} alt='IFSUl Logo Image' className='h-12' />
+          </li>
+          <li className='text-color-text font-custom mb-4 md:mb-0'>@Aprendendo Sem Bullying</li>
+          <li className='text-color-text font-custom mb-4 md:mb-0'>Sobre Nós</li>
+          <li className='text-color-text font-custom mb-4 md:mb-0'>Política de Privacidade</li>
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- aprendendo-sem-bullying/src/components/Header.tsx
+++ aprendendo-sem-bullying/src/components/Header.tsx
@@ -4,21 +4,23 @@ import logoImage from '../public/logo_project.png'
 const Header = () => {
   return (
     <div>
-      <div className='bg-header flex flex-col items-center'>
-        <img src={logoImage} alt='Logo do projeto'/>
-        <div>
-          <ul className='flex flex-row justify-between space-x-4'>
-            <li className='bg-button text-button-text rounded-xl p-3'>INÍCIO</li>
-            <li className='bg-button text-button-text rounded-lg p-3'>TIPOS</li>
-            <li className='bg-button text-button-text rounded-lg p-3'>SINAIS</li>
-            <li className='bg-button text-button-text rounded-lg p-3'>EFEITOS</li>
-            <li className='bg-button text-button-text rounded-lg p-3'>PREVENÇÃO</li>
-            <li className='bg-button text-button-text rounded-lg p-3'>LEGISLAÇÃO</li>
-            <li className='bg-button text-button-text rounded-lg p-3'>INTERVENÇÃO</li>
-            <li className='bg-button text-button-text rounded-lg p-3'>DEPOIMENTOS</li>
-          </ul>
+      <div className='bg-header flex flex-col items-center pt-7 pb-7'>
+        <div className='flex flex-col items-center justify-center'>
+          <img src={logoImage} alt='Logo do projeto' className='md:w-48 lg:w-64 xl:w-96 max-w-full'/>
         </div>
-      </div>
+        <div className='pt-7  flex-wrap'>
+          <ul className='flex flex-col md:flex-row justify-center md:justify-between space-y-4 md:space-y-0 md:space-x-4'>
+            <li className='bg-button text-color-text rounded-custom py-2 px-3 md:py-3 md:px-5 font-custom text-sm md:text-base lg:text-lg xl:text-xl items-center'>INÍCIO</li>
+            <li className='bg-button text-color-text rounded-custom py-2 px-3 md:py-3 md:px-5 font-custom text-sm md:text-base lg:text-lg xl:text-xl items-center'>TIPOS</li>
+            <li className='bg-button text-color-text rounded-custom py-2 px-3 md:py-3 md:px-5 font-custom text-sm md:text-base lg:text-lg xl:text-xl items-center'>SINAIS</li>
+            <li className='bg-button text-color-text rounded-custom py-2 px-3 md:py-3 md:px-5 font-custom text-sm md:text-base lg:text-lg xl:text-xl items-center'>EFEITOS</li>
+            <li className='bg-button text-color-text rounded-custom py-2 px-3 md:py-3 md:px-5 font-custom text-sm md:text-base lg:text-lg xl:text-xl items-center'>PREVENÇÃO</li>
+            <li className='bg-button text-color-text rounded-custom py-2 px-3 md:py-3 md:px-5 font-custom text-sm md:text-base lg:text-lg xl:text-xl items-center'>LEGISLAÇÃO</li>
+            <li className='bg-button text-color-text rounded-custom py-2 px-3 md:py-3 md:px-5 font-custom text-sm md:text-base lg:text-lg xl:text-xl items-center'>INTERVENÇÃO</li>
+            <li className='bg-button text-color-text rounded-custom py-2 px-3 md:py-3 md:px-5 font-custom text-sm md:text-base lg:text-lg xl:text-xl items-center'>DEPOIMENTOS</li>
+          </ul>
+        </div>        
+      </div>      
     </div>
   )
 }
